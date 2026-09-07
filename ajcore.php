014daf37199a8c1d4c7651d329d82cf8b279edc6
sandman <ssn.jnk@gmail.com>
2026-09-06 22:48:11 -0400

    Release AJ Core 0.7.289

--- ajcore.php
+++ ajcore.php
@@ -3,7 +3,7 @@
  * Plugin Name:       AJ Core
  * Plugin URI:        https://github.com/ssnanda/ajcore
  * Description:       A modular WordPress business toolkit for forms, payments, portals, auth, CRM, and automations.
- * Version: 0.7.288
+ * Version: 0.7.289
  * Author:            IT Spector LLC
  * Author URI:        https://itspector.com
  * Update URI:        false
@@ -18,7 +18,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 if ( ! defined( 'AJCORE_VERSION' ) ) {
-	define( 'AJCORE_VERSION', '0.7.288' );
+	define( 'AJCORE_VERSION', '0.7.289' );
 }
 
 if ( ! defined( 'AJCORE_PLUGIN_DIR' ) ) {
